7452d101612564222704b6875423f15c89e1765a
josesantiz-dev <JOSE_SANTIZR1990@HOTMAIL.COM>
2022-07-21 15:48:15 -0500

    seconds release

---
+++ Assets/js/functions_generaciones.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function(){
+    tableGeneraciones = $('#tableGeneraciones').dataTable( {
+		"aProcessing":true,
+		"aServerSide":true,
+        "language": {
+        	"url": " "+base_url+"/Assets/plugins/Spanish.json"
+        },
+        "ajax":{
+            "url": " "+base_url+"/GeneracionJose/getListaGeneraciones",
+            "dataSrc":""
+        },
+        "columns":[
+            {"data":"id"},
+            {"data":"nombre_generacion"},
+            {"data":"fecha_inicio_gen"},
+            {"data":"fecha_fin_gen"},
+            {"data":"estatus"},
+            {"data":"id"}
+        ],
+        "responsive": true,
+	    "paging": true,
+	    "lengthChange": true,
+	    "searching": true,
+	    "ordering": true,
+	    "info": true,
+	    "autoWidth": false,
+	    "scrollY": '44vh',
+	    "scrollCollapse": true,
+	    "bDestroy": true,
+	    "order": [[ 0, "asc" ]],
+	    "iDisplayLength": 25
+    }); 
+    $('#tableGeneraciones').DataTable();
+})
