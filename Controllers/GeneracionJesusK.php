0562697e2f467b5c679e0740c9c7489e5e667c46
JESUS-K <kerlyjesus98@outlook.com>
2022-07-22 12:15:46 -0500

    actualizacion9

---
+++ Controllers/GeneracionJesusK.php
@@ -0,0 +1,36 @@
+<?php
+    class GeneracionJesusK extends Controllers{
+
+        private $idUser;
+        public function __construct()
+        {
+            parent::__construct();
+           /* session_start();
+            if(empty($_SESSION['login']))
+            {
+                header('Location: '.base_url().'/login');
+                die();
+            }
+            $this->idUser = $_SESSION['idUser'];
+            */
+        }
+        public function getGeneraciones()
+        {
+            //$data = "";
+            $data ['page_functions_js'] = "functions_generaciones.js";
+         $this->views->getView($this, "generacion", $data);
+
+         //$arrGeneraciones = $this -> model -> selectGeneraciones();
+         //var_dump ($arrGeneraciones); 
+         //echo (json_encode($arrGeneraciones, JSON_UNESCAPED_UNICODE));
+
+        }
+
+        public function getListaGeneraciones()
+        {
+        $arrGeneraciones = $this -> model -> selectGeneraciones();
+        echo (json_encode($arrGeneraciones, JSON_UNESCAPED_UNICODE));
+        }
+    }
+
+?>
